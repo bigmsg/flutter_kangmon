7bffaf9c79f75f4bd60a9e2da600900cebfe119f
bigmsg <travelcar@naver.com>
2020-02-18 10:49:54 +0900

    Initial commit

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('강몬'),
+      ),
+      body: Text('강사개인코칭 플랫폼'),
+    );
+  }
+}
