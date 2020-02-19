cfa2c74966baf249132ac24087f513a1604c0e0e
bigmsg <travelcar@naver.com>
2020-02-19 15:18:53 +0900

    통신하기

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter_kangmon/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
+
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:rxdart/rxdart.dart';
+import 'dart:convert';
+
+
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,11 +11,47 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  Future<String> getData() async {
+    /*Future<String> response = await http.get(
+      Uri.encodeFull('https://'),
+      headers: {'Accept': 'application/json'}
+    );*/
+    //var response = await http.get('https://www.massagemania.co.kr/_mobileapp/admin/test.php', body: {});
+    /*var response = await http.post('https://www.massagemania.co.kr/_mobileapp/admin/test.php', body: {'mb_id': 'mania'});
+    print(response.body);
+
+
+     */
+    var response = await http.post('https://www.massagemania.co.kr/_mobileapp/admin/test.php',
+        body: {'mb_id': 'mania'}).then(
+        (http.Response response) {
+          final int statusCode = response.statusCode;
+          if (statusCode < 200 || statusCode > 400 || json == null) {
+            throw new Exception("Error while fetching data");
+          }
+          print(response.body);
+          //return json.decode(response.body);
+        });
+    print(response);
+    return '00';
+  }
+
+  /*Future getData() async {
+
+  }*/
+
+  @override
+  initState() {
+    super.initState();
+    this.getData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('강몬 작업시작'),
+        title: Text('강몬'),
         leading: IconButton(
           icon: Icon(Icons.account_circle),
           iconSize: 30.0,
@@ -24,7 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 fontSize: 20.0,
               ),
             ),
-            onPressed: () {},
+            onPressed: getData,
           ),
         ],
       ),
